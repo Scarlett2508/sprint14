f6634d225852e93679d473b4d10e9fc7522d5fb9
Alevtina <fedinaalevtina@gmail.com>
2020-05-28 17:39:06 +0300

    added password and email

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,6 +23,20 @@ const userSchema = new mongoose.Schema({
         return validator.isURL(valid);
       },
       message: 'Неверный формат URL',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
     },
   },
 });
