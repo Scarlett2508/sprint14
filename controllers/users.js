0f02b9e7a9a5fd94172aa8f0adc81612118a8ce5
Alevtina <fedinaalevtina@gmail.com>
2020-05-31 22:39:38 +0300

    added CastError

--- controllers/users.js
+++ controllers/users.js
@@ -23,7 +23,10 @@ module.exports.createUser = (req, res) => {
       });
     })
     .catch((err) => {
-      if (err.name === err.ValidationError) {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === 'CastError') {
         return res.status(400).send({ message: err.message });
       }
       return res.status(500).send({ message: 'Произошла ошибка' });
