fcb832f9e62b5f25ebf69c9028a8d9d4ecfa9eb1
Alevtina <fedinaalevtina@gmail.com>
2020-06-14 11:42:40 +0300

    Joi + celebrate

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
+
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { Error } = require('./middlewares/error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,6 +23,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}), login);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
@@ -31,5 +41,7 @@ app.use('/', cardRoutes);
 app.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
+
+app.use(Error);
 
 app.listen(PORT);
---
+++ middlewares/error.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Error = (err, req, res, next) => {
+
+  const { statusCode = 500, message } = err;
+
+  res
+      .status(statusCode)
+      .send({
+
+          message: statusCode === 500
+              ? 'На сервере произошла ошибка'
+              : message
+      });
+});
